b82c99497cad1cd7e347467297d83628fe24dbb9
Eric C <erickabanes@gmail.com>
2024-02-12 16:34:55 +0100

    Primera subida

--- src/index.js
+++ src/index.js
@@ -1,17 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+//Variables y constantes
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const nombre = "Eric";
+let apellido = "Cano";
+
+let valorDado = 5
+
+console.log(nombre,apellido,valorDado)
